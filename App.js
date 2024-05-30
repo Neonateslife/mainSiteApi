f269f7ea5c81f7d161295aaae02da372e3a51989
Opini Isaac <65159215+OpiniIsaac@users.noreply.github.com>
2024-05-30 20:03:39 +0300

    added get payment status

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ const momoHost = 'proxy.momoapi.mtn.com';
 const momoTokenUrl = `https://${momoHost}/collection/token/`;
 const momoRequestToPayUrl = `https://${momoHost}/collection/v1_0/requesttopay`;
 
+const momoPaymentStatusUrl = `https://${momoHost}/collection/v2_0/payment/`;
 let momoToken = null;
 var encodedData = base64.encode( "491bece6-0474-4e71-9209-ac54bb653edd:e4e3fcbceeee4f4a8bca9ac334e5ce25");
 console.log("the encoded is " + encodedData);
@@ -123,6 +124,7 @@ app.post('/pay', async (req, res) => {
 
     // Step 2: Make request to pay
     const referenceId = uuidv4();
+    console.log(referenceId);
     const body = {
       amount: total,
       currency: 'UGX',
@@ -140,7 +142,7 @@ app.post('/pay', async (req, res) => {
       body,
       {
         headers: {
-          'X-Reference-Id': uuidv4(),
+          'X-Reference-Id': referenceId ,
           'X-Target-Environment': 'mtnuganda',
           'Ocp-Apim-Subscription-Key': '04a79371a8834513a4031e5f4bf0778f',
           Authorization: `Bearer ${momoToken}`,
@@ -156,6 +158,45 @@ app.post('/pay', async (req, res) => {
   }
 });
 
+// Endpoint to get payment status
+app.get('/payment-status/:referenceId', async (req, res) => {
+  try {
+    const { referenceId } = req.params;
+
+    // Step 1: Get MoMo token
+    const tokenResponse = await axios.post(
+      momoTokenUrl,
+      {},
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Ocp-Apim-Subscription-Key': '04a79371a8834513a4031e5f4bf0778f',
+          Authorization: `Basic ${encodedData}`,
+        },
+      }
+    );
+
+    const momoToken = tokenResponse.data.access_token;
+
+    // Step 2: Get payment status
+    const paymentStatusResponse = await axios.get(
+      `${momoPaymentStatusUrl}${referenceId}`,
+      {
+        headers: {
+          'X-Target-Environment': 'mtnuganda',
+          'Ocp-Apim-Subscription-Key': '04a79371a8834513a4031e5f4bf0778f',
+          Authorization: `Bearer ${momoToken}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    res.json(paymentStatusResponse.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
 
 
 
